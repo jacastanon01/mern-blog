4d32a151daa34edc9825bdf50f46820848cb2d2a
Jacob Castanon <jacastanon01@gmail.com>
2023-08-03 16:06:45 -0500

    deploy test

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "path"
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
@@ -8,33 +8,31 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import { connectDB } from "./api/connectDB.js";
 dotenv.config();
 
-connectDB()
+connectDB();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 //app.use("/api/users", userRoutes);
-app.use("/api/users", userRoutes)
-app.use("/api/blogs", blogRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/blogs", blogRoutes);
 
-
-if (process.env.NODE_ENV === "production"){
-  const __dirname = path.resolve()
-  app.use(express.static(path.join(__dirname, "frontend/dist")))
-  
-  app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  })
+if (process.env.NODE_ENV === "production") {
+  const __dirname = "/opt/render/project/src";
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  console.log("DIRNAME: ", __dirname, path);
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
-    res.send("hello from the server!")
+    res.send("hello from the server!");
   });
 }
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("connected to port " + process.env.PORT);
--- deployment.md
+++ deployment.md
@@ -1,7 +1,7 @@
 1. set script for deployment - done
 2. initialize git - done
-3. create repo on github
-4. push code to github
+3. create repo on github - done
+4. push code to github -done
 5. connect gh repo to render
 6. Configure build settings on render
 7. Set environment variables on render
