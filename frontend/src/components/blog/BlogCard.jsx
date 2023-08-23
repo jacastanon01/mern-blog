e28ba8d47eafda5810ff54e7ddb8d3b4b4b7970f
Jacob Castanon <jacastanon01@gmail.com>
2023-08-22 20:14:15 -0500

    reafctored blog card into reusable component

--- frontend/src/components/blog/BlogCard.jsx
+++ frontend/src/components/blog/BlogCard.jsx
@@ -4,54 +4,29 @@ import { FaRegTrashAlt, FaEdit } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import { formatISO9075 } from "date-fns";
 
-function BlogCard({ post, author }) {
+function BlogCard({
+  post,
+  author,
+  styles,
+  title,
+  body,
+  // ...cardStyles
+}) {
   //const styles = localStorage.getItem("userInfo") ? { flex: 1 } : null;
+  const { cardStyles, titleStyles, bodyStyles } = styles;
   console.log("POST", post);
   return (
     post && (
-      <Card
-        style={{
-          // border: "0.2em double #eee",
-          outline: "none",
-          height: "100%",
-          boxShadow: "rgba(255, 255, 255, 0.1) 4px 4px 12px 12px",
-        }}
-      >
-        <Card.Title
-          className="text-center"
-          style={{
-            borderBottom: "1px solid white",
-            paddingBottom: "0.5em",
-            background: "var(--primary)",
-          }}
-        >
-          <div className="d-flex flex-column justify-content-center align-items-center mt-3">
-            <h1 className="text-uppercase">{post?.title}</h1>
-            <div className="mb-2 w-50 ms-auto" style={{ fontSize: "0.7em" }}>
-              <span>Written by:</span>
-              <LinkContainer
-                to={`../user/${post.author._id}`}
-                style={{ cursor: "pointer" }}
-              >
-                <span className=" ms-2 text-capitalize text-decoration-underline">
-                  {post.author.name}
-                </span>
-              </LinkContainer>
-              <time style={{ fontSize: "0.7rem", display: "block" }}>
-                {formatISO9075(new Date(post?.createdAt))}
-              </time>
-            </div>
-          </div>
+      <Card style={cardStyles}>
+        <Card.Title className="text-center" style={titleStyles}>
+          {title}
         </Card.Title>
 
         <Card.Body
-          style={{
-            height: "30vh",
-            fontSize: "calc(1.35em + 1vw)",
-          }}
+          style={bodyStyles}
           // className="d-flex flex-column justify-content-center"
         >
-          {post?.body}
+          {body}
         </Card.Body>
       </Card>
     )
@@ -64,9 +39,6 @@ function BlogCardTitle({ children, ...styles }) {
       className="text-center"
       style={{
         ...styles,
-        borderBottom: "1px solid white",
-        paddingBottom: "0.5em",
-        background: "var(--primary)",
       }}
     >
       {children}
--- frontend/src/pages/blogPages/SingleBlog.jsx
+++ frontend/src/pages/blogPages/SingleBlog.jsx
@@ -1,13 +1,7 @@
-import { Form, Row, Col, Button, Card, Container } from "react-bootstrap";
-import { useState, useEffect } from "react";
-
+import { formatISO9075 } from "date-fns";
 import FormContainer from "../../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  useUpdateBlogMutation,
-  useGetSingleBlogQuery,
-  useDeleteBlogMutation,
-} from "../../redux/slices/blogsApiSlice";
+import { useGetSingleBlogQuery } from "../../redux/slices/blogsApiSlice";
 import { useNavigate, useParams } from "react-router-dom";
 import { LoadingSpinner } from "../../components/LoadingSpinner";
 import { LinkContainer } from "react-router-bootstrap";
@@ -18,11 +12,44 @@ import EditForm from "../../components/blog/EditForm";
 
 function SingleBlog() {
   const { blogId } = useParams();
-  const { data, isLoading, error } = useGetSingleBlogQuery(
-    { blogId },
-    { refetchOnMountOrArgChange: true }
-  );
+  const { data, isLoading, error } = useGetSingleBlogQuery({ blogId });
   const { userInfo } = useSelector((state) => state.auth);
+
+  const cardStyles = {
+    // border: "0.2em double #eee",
+    outline: "none",
+    height: "100%",
+    boxShadow: "rgba(255, 255, 255, 0.1) 4px 4px 12px 12px",
+  };
+  const titleStyles = {
+    borderBottom: "1px solid white",
+    paddingBottom: "0.5em",
+    background: "var(--primary)",
+  };
+  const bodyStyles = {
+    height: "30vh",
+    fontSize: "calc(1.35em + 1vw)",
+  };
+
+  const titleContent = data && (
+    <div className="d-flex flex-column justify-content-center align-items-center mt-3">
+      <h1 className="text-uppercase">{data?.post.title}</h1>
+      <div className="mb-2 w-50 ms-auto" style={{ fontSize: "0.7em" }}>
+        <span>Written by:</span>
+        <LinkContainer
+          to={`../user/${data?.post.author._id}`}
+          style={{ cursor: "pointer" }}
+        >
+          <span className=" ms-2 text-capitalize text-decoration-underline">
+            {data?.post.author.name}
+          </span>
+        </LinkContainer>
+        <time style={{ fontSize: "0.7rem", display: "block" }}>
+          {formatISO9075(new Date(data?.post.createdAt))}
+        </time>
+      </div>
+    </div>
+  );
 
   if (isLoading) return <LoadingSpinner />;
   if (error || !blogId) return <p>{error}</p>;
@@ -30,7 +57,12 @@ function SingleBlog() {
     data?.post.author && (
       <FormContainer>
         {userInfo.email !== data.post.author.email ? (
-          <BlogCard post={data.post} />
+          <BlogCard
+            post={data.post}
+            styles={{ cardStyles, titleStyles, bodyStyles }}
+            title={titleContent}
+            body={data.post.body}
+          />
         ) : (
           <EditForm />
         )}
